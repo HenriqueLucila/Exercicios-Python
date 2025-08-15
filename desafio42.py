ea6e554d66e74bf39f753b36c61cfb94c1260893
Henrique Lucila <henriquebiciatolucila@gmail.com>
2025-08-15 14:51:41 -0300

    Create desafio42.py

---
+++ desafio42.py
@@ -0,0 +1,23 @@
+"""
+Refaça o desafio 35 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+- Equilátero: todos os lados iguais
+- Isósceles: dois lados iguais
+- Escaleno: todos os lados diferentes
+"""
+
+lado1 = int(input("Tamanho do lado 1: "))
+lado2 = int(input("Tamanho do lado 2: "))
+lado3 = int(input("Tamanho do lado 3: "))
+
+if (lado1 + lado2) > lado3 and (lado1 + lado3) > lado2 and (lado2 + lado3) > lado1:
+    print("É possível formar um triângulo")
+
+    if lado1 == lado2 and lado1 == lado3:
+        print("O triângulo formado será EQUILÁTERO")
+    elif (lado1 == lado2 and lado1 != lado3) or (lado1 == lado3 and lado1 != lado2) or (lado2 == lado3 and lado2 != lado1):
+        print("O triângulo formado será ISÓSCELES")
+    elif lado1 != lado2 and lado1 != lado3 and lado2 != lado3:
+        print("O triângulo formado será ESCALENO")
+
+else:
+    print("Não é possível formar um triângulo")
