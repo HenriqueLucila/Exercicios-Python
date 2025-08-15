77b7dd583fe4ead3663fa17c5cc410dfbea1b000
Henrique Lucila <henriquebiciatolucila@gmail.com>
2025-08-15 14:17:35 -0300

    Create desafio40.py

---
+++ desafio40.py
@@ -0,0 +1,17 @@
+"""
+Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida:
+- Média abaixo de 5.0: Reprovado
+- Média entre 5.0 e 6.9: Recuperação
+- Média 7.0 ou superior: Aprovado
+"""
+n1 = float(input("Nota 1: "))
+n2 = float(input("Nota 2: "))
+
+media = (n1 + n2) / 2
+
+if media < 5.0:
+    print("Média: {:.2f} \nSituação final: REPROVADO.".format(media))
+elif media > 5.0 and media < 7.0:
+    print("Média: {:.2f} \nSituação final: RECUPERAÇÃO.".format(media))
+elif media >= 7.0:
+    print("Média: {:.2f} \nSituação final: APROVADO.".format(media))
