88af07e270b363c6c28bf709a5243da73727e32f
Henrique Lucila <henriquebiciatolucila@gmail.com>
2025-08-18 12:18:20 -0300

    Update desafio42.py

--- desafio42.py
+++ desafio42.py
@@ -12,12 +12,12 @@ lado3 = int(input("Tamanho do lado 3: "))
 if (lado1 + lado2) > lado3 and (lado1 + lado3) > lado2 and (lado2 + lado3) > lado1:
     print("É possível formar um triângulo")
 
-    if lado1 == lado2 and lado1 == lado3:
+    if lado1 == lado2 == lado3:
         print("O triângulo formado será EQUILÁTERO")
-    elif (lado1 == lado2 and lado1 != lado3) or (lado1 == lado3 and lado1 != lado2) or (lado2 == lado3 and lado2 != lado1):
-        print("O triângulo formado será ISÓSCELES")
-    elif lado1 != lado2 and lado1 != lado3 and lado2 != lado3:
+    elif lado1 != lado2 != lado3 != lado1:
         print("O triângulo formado será ESCALENO")
+    else:
+        print("O triângulo formado será ISÓSCELES")
 
 else:
     print("Não é possível formar um triângulo")
